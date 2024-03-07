c571d0636e109ff16f4d9ce6b7636e0dde25a4a4
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-07 11:48:33 +0800

    feat: deleted some files and renaming some files

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -20,3 +20,13 @@ class CommandTest(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(database='default')
+    
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+        patched_check.side_effect = [pg_error] * 2 + [OperationalError] * 3 + True
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count,6)
+        patched_check.assert_called_with(database='default')
