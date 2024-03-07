be84032819476edfcc2d89216bb521a3ac773125
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-08 00:07:25 +0800

    feat: adding test for editing and adding user page, and make necessary changes in admin.py

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -34,3 +34,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_edit_page(self):
+        """Test user edit page if working correctly."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_user_add_page(self):
+        """Test user add page if working correctly."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
