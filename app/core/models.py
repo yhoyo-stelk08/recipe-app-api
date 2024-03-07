5dfaebaf2d54e45ae6d511fd060e84333758cdb7
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-07 14:15:09 +0800

    feat: make custom user models using AbstractBaseUser class in Django

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,18 @@
-# from django.db import models
+"""
+Custom user model
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Make custom user model extends from AbstractBaseUser"""
+
+    # declare fields
+    email = models.EmailField(unique=True,max_length=100)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
