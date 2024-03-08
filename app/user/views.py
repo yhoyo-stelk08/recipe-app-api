aa8ac08200c275c9b37a125feb628a144b394a28
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-08 17:46:16 +0800

    feat: make new serializers in user app serializers.py for generate auth token, then add CBV in views for generate token for user
    modified:   app/app/settings.py
    modified:   app/user/serializers.py
    modified:   app/user/urls.py
    modified:   app/user/views.py

--- app/user/views.py
+++ app/user/views.py
@@ -3,9 +3,21 @@ View for User API
 """
 
 from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create new user in the system """
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Generate token for the valid users."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
