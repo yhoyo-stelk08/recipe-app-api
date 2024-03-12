26769d31dd03340911286c1701978dabc322f3e1
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-12 22:37:44 +0800

    feat: make necessary changes in views for pass the test
     Changes to be committed:
    	modified:   app/recipe/views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -21,6 +21,17 @@ from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Filter by items assigned to recipes.'
+            )
+        ]
+    )
+)
 class BaseClassRecipeAttrViewset(
         mixins.DestroyModelMixin,
         mixins.UpdateModelMixin,
@@ -31,7 +42,16 @@ class BaseClassRecipeAttrViewset(
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
 
 @extend_schema_view(
